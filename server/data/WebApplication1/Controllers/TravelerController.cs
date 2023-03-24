ae2d3fb4e6685636ed315e5e1c26144f00f670a9
lishayp <LISHAY.PELED@GMAIL.COM>
2023-03-24 18:46:35 +0300

    db

--- server/data/WebApplication1/Controllers/TravelerController.cs
+++ server/data/WebApplication1/Controllers/TravelerController.cs
@@ -8,34 +8,65 @@ using DATAa;
 
 namespace WebApplication1.Controllers
 {
-   
-    public class TravelerController : ApiController
+    public class travelerController : ApiController
     {
-        project_testEntities db = new project_testEntities();
-        // GET: api/Traveler
-        public IEnumerable<tblTravelere> Get()
+        igroup190_test1Entities db = new igroup190_test1Entities();
+        // GET: api/traveler
+        public IEnumerable<travelere> Get()
         {
-            List<tblTravelere> travelers = db.tblTraveleres.ToList();
+            List<travelere> travelers = db.traveleres.ToList();
             return travelers;
         }
 
-        // GET: api/Traveler/5
+        // GET: api/traveler/5
         public string Get(int id)
         {
             return "value";
         }
+        [HttpPost]
+        [Route("api/post/SignUp")]
+        public IHttpActionResult PostSignUp([FromBody] travelere value)
+        {
+            try
+            {
+                // create a new traveler user
+                travelere newTraveler = new travelere
+                {
+                    first_name = value.first_name,
+                    last_name = value.last_name,
+                    //password = value.password,
+                    travler_email = value.travler_email,
+                    phone = value.phone,
+                    dateOfBirth = value.dateOfBirth,
+                    gender = value.gender,
+                    insurence_company = value.insurence_company,
+                    notifications = value.notifications,
+                    location = value.location,
+                    save_location = value.save_location
 
-        // POST: api/Traveler
-        public void Post([FromBody]string value)
+                };
+
+                db.traveleres.Add(newTraveler);
+                db.SaveChanges();
+                return Ok("Traveler created successfully.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST: api/traveler
+        public void Post([FromBody] string value)
         {
         }
 
-        // PUT: api/Traveler/5
-        public void Put(int id, [FromBody]string value)
+        // PUT: api/traveler/5
+        public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE: api/Traveler/5
+        // DELETE: api/traveler/5
         public void Delete(int id)
         {
         }
