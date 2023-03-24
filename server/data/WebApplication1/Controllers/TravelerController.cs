40b453ad7a04859e34dd112e39ac68129e48eb08
lishayp <LISHAY.PELED@GMAIL.COM>
2023-03-24 20:01:49 +0300

    Update TravelerController.cs

--- server/data/WebApplication1/Controllers/TravelerController.cs
+++ server/data/WebApplication1/Controllers/TravelerController.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,64 +8,34 @@ using DATAa;
 
 namespace WebApplication1.Controllers
 {
-    public class travelerController : ApiController
+   
+    public class TravelerController : ApiController
     {
-        igroup190_test1Entities db = new igroup190_test1Entities();
-        // GET: api/traveler
-        public IEnumerable<travelere> Get()
+       igroup190_project db = new igroup190_project();
+        // GET: api/Traveler
+        public IEnumerable<traveleres> Get()
         {
-            List<travelere> travelers = db.traveleres.ToList();
+            List<traveleres> travelers = db.traveleres.ToList();
             return travelers;
         }
 
-        // GET: api/traveler/5
+        // GET: api/Traveler/5
         public string Get(int id)
         {
             return "value";
         }
-        [HttpPost]
-        [Route("api/post/SignUp")]
-        public IHttpActionResult PostSignUp([FromBody] travelere value)
-        {
-            try
-            {
-                // create a new traveler user
 
-                db.traveleres.Add((travelere)new travelere
-                {
-                    first_name = value.first_name,
-                    last_name = value.last_name,
-                    //password = value.password,
-                    travler_email = value.travler_email,
-                    phone = value.phone,
-                    dateOfBirth = value.dateOfBirth,
-                    gender = value.gender,
-                    insurence_company = value.insurence_company,
-                    notifications = value.notifications,
-                    location = value.location,
-                    save_location = value.save_location
-
-                });
-                db.SaveChanges();
-                return Ok("Traveler created successfully.");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
-        // POST: api/traveler
-        public void Post([FromBody] string value)
+        // POST: api/Traveler
+        public void Post([FromBody]string value)
         {
         }
 
-        // PUT: api/traveler/5
-        public void Put(int id, [FromBody] string value)
+        // PUT: api/Traveler/5
+        public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE: api/traveler/5
+        // DELETE: api/Traveler/5
         public void Delete(int id)
         {
         }
