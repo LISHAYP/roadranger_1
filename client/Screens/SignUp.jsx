a2f800b032972c4b15974bb38ab0edbd987a176c
lishayp <LISHAY.PELED@GMAIL.COM>
2023-03-25 12:39:03 +0300

    chat

--- client/Screens/SignUp.jsx
+++ client/Screens/SignUp.jsx
@@ -62,6 +62,7 @@ export default function SignUp() {
           dateOfBirth: isCalendarOpen,
           gender: gender,
           password: password,
+          chat: isEnabledChatMode
         })
       });
 
