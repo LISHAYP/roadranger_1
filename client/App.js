f07dcd5310a92dc4b948234bd9b2b9676e6fe425
Orian Nahari <orian.nahari1@gmail.com>
2023-03-27 11:32:08 +0300

    Maps

--- client/App.js
+++ client/App.js
@@ -5,6 +5,7 @@ import SignUp from './Screens/SignUp';
 import AroundYou from './Screens/AroundYou';
 import Setting from './Screens/Setting'
 import ContactUs from './Screens/ContactUs';
+
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native';
 import { Picker } from '@react-native-picker/picker';
@@ -13,17 +14,18 @@ import ForgotPassword from './Screens/ForgotPassword';
 const Stack = createNativeStackNavigator()
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Sign In">
-        <Stack.Screen name="Sign In" component={SignIn} />
-        <Stack.Screen name="Sign Up" component={SignUp} />
-        <Stack.Screen name="Around You" component={AroundYou} />
-        <Stack.Screen name="Contact Us" component={ContactUs} />
-        <Stack.Screen name="Forgot password" component={ForgotPassword} />
-        <Stack.Screen name="Setting" component={Setting} />
+    <AroundYou/>
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="Sign In">
+    //     <Stack.Screen name="Sign In" component={SignIn} />
+    //     <Stack.Screen name="Sign Up" component={SignUp} />
+    //     <Stack.Screen name="Around You" component={AroundYou} />
+    //     <Stack.Screen name="Contact Us" component={ContactUs} />
+    //     <Stack.Screen name="Forgot password" component={ForgotPassword} />
+    //     <Stack.Screen name="Setting" component={Setting} />
 
-      </Stack.Navigator> 
-    </NavigationContainer>
+    //   </Stack.Navigator> 
+    // </NavigationContainer>
   );
 }
 
--- client/Screens/AroundYou.jsx
+++ client/Screens/AroundYou.jsx
@@ -1,23 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+import  { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
 
-export default function AroundYou(props) {
-    const email = props.route.params.email;
+
+export default function App() {
+    const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission denied');
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text>email: {email}</Text>
+      {location && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="My Location"
+            description="This is my current location"
+          />
+        </MapView>
+      )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-        marginVertical: 10,
-        marginHorizontal: 10,
-        padding: 20,
-        width: "100%",
-
-    },
-})
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
