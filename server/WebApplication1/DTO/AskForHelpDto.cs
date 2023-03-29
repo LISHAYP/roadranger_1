0b14e8cc700217040a776d56fd5ebb3446b352e1
lishayp <LISHAY.PELED@GMAIL.COM>
2023-03-29 17:16:06 +0300

    שינויים ליאל

---
+++ server/WebApplication1/DTO/AskForHelpDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.DTO
+{
+    public class AskForHelpDto
+    {
+        public int RequestNumber { get; set; }
+        public string Details { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public string Picture { get; set; }
+        public int UserId { get; set; }
+        public int SerialTypeNumber { get; set; }
+        public int CountryNumber { get; set; }
+        public int AreaNumber { get; set; }
+        public string TypeName { get; set; }
+        public string CountryName { get; set; }
+        public string AreaName { get; set; }
+    }
+}
