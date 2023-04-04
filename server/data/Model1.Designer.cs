57d7c76525a89ce7049b6ee572ac9078c39f6aa1
lishayp <LISHAY.PELED@GMAIL.COM>
2023-04-04 20:28:43 +0300

    *

--- server/data/Model1.Designer.cs
+++ server/data/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\USER\OneDrive\שולחן העבודה\מנהל עסקים\שנה ג\פרויקט גמר מערכות מידע\git_project\roadranger_1\server\data\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\97250\Desktop\פרויקט גמר\roadranger_1\server\data\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
