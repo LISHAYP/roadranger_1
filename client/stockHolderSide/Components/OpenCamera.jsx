be027900c52672d89e3fc1c0e4e7d61a425448a6
lishayp <LISHAY.PELED@GMAIL.COM>
2023-04-08 19:38:56 +0300

    08/04/23  19:30

---
+++ client/stockHolderSide/Components/OpenCamera.jsx
@@ -0,0 +1,197 @@
+import { Camera, CameraType } from 'expo-camera';
+import React, { useState } from 'react';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons";
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function OpenCamera(props) {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [camera, setCamera] = useState(null);
+  const navigation = useNavigation();
+  const [image, setImage] = useState(null);
+  const [animate, setAnimate] = useState(false);
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
+  const takePicture = async () => {
+    if (camera) {
+      try {
+        const photo = await camera.takePictureAsync({
+          quality: 0.1,
+          base64: true,
+        });
+        setImage(photo);
+
+        const pic64base = photo.base64;
+        const picName64base = `user_${new Date().getTime()}.jpg `;
+        const picUri = `data:image/gif;base64,${photo.base64}`;
+
+        // Add the following lines to call the uploadBase64ToASMX function
+        setAnimate(true);
+        let urlAPI = 'http://cgroup90@194.90.158.74/cgroup90/prod/profilePictures/';
+        fetch(urlAPI, {
+          method: 'POST',
+          body: JSON.stringify({
+            base64img: pic64base,
+            base64imgName: picName64base,
+          }),
+        })
+          .then((response) => response.json())
+          .then((responseJson) => {
+            console.log(responseJson);
+            setAnimate(false);
+          })
+          .catch((error) => {
+            console.error(error);
+            setAnimate(false);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+
+  const openGallery = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log({ pickerResult }); // Log the pickerResult object
+    if (pickerResult.canceled || pickerResult.cancelled) {
+      console.log('Image selection cancelled'); // Handle cancel event
+    } else {
+      const selectedAsset = pickerResult.assets[0];
+      const image = { uri: selectedAsset.uri, width: selectedAsset.width, height: selectedAsset.height };
+      console.log({ image });
+      setImage(image);
+    }
+  };
+  const savePhoto = () => {
+    console.log('img',);
+
+    navigation.goBack();
+  };
+  const closeCamera = () => {
+    navigation.goBack(); // navigate to the previous screen
+  }
+
+  return (
+    <View style={styles.container}>
+      {image ? (
+        <View style={styles.preview}>
+          <Image style={styles.previewImage} source={{ uri: image.uri }} />
+          <TouchableOpacity style={styles.previewButton} onPress={() => setImage(null)}>
+            <Icon name="close-outline" size={35} color='white' />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.save} onPress={savePhoto} >
+            <Icon name="download-outline" size={35} />
+            <Text style={styles.textSave}>Save</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <Camera style={styles.camera} type={type} ref={(ref) => { setCamera(ref) }}>
+          <TouchableOpacity style={styles.buttonClose} onPress={closeCamera}>
+            <Icon name="close-outline" size={35} color='white' />
+          </TouchableOpacity>
+          <View style={styles.buttonContainer} >
+            <TouchableOpacity style={styles.button} onPress={openGallery}>
+              <Icon name="images-outline" size={45} color='white' style={styles.iconLeft} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={takePicture}>
+              <Icon name="radio-button-on-outline" size={100} color='white' style={styles.iconCenter} />
+
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+              <Icon name="sync-circle-outline" size={45} color='white' style={styles.iconRight} />
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  buttonClose: {
+    position: 'absolute',
+    top: 32,
+    right: 16
+  },
+  iconRight: {
+    left: 40
+  },
+  iconLeft: {
+    right: 40
+  },
+  iconCenter: {
+    bottom: 30
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  previewImage: {
+    width: '100%',
+    height: '100%',
+  },
+  previewButton: {
+    position: 'absolute',
+    top: 32,
+    right: 16
+  },
+  save: {
+    flexDirection: 'row',
+    marginBottom: 50,
+  },
+  textSave: {
+    fontSize: 25,
+    marginTop: 10
+
+  }
+});
---
+++ client/travelersSide/Screens/NewEvent.jsx
@@ -0,0 +1,337 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image, ScrollView, Switch } from 'react-native';
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Ionicons";
+import RoadRanger from '../assets/RoadRanger.png';
+import { Dropdown } from 'react-native-element-dropdown';
+import GradientBackground from '../Components/GradientBackground';
+import Geocoder from 'react-native-geocoding';
+import { useEffect } from 'react';
+
+
+export default function NewEvent(props) {
+  const traveler = props.route.params.traveler;
+  const userLocation = props.route.params.userLocation
+  const navigation = useNavigation();
+  const [country, setCountry] = useState('');
+  const [region,setRegion]=useState('');
+  const serialType = [
+    //creating type of different eventtypes
+    { label: 'Weather', value: '1' },
+    { label: 'Car Accidents', value: '2' },
+    { label: 'Road closures', value: '3' },
+    { label: 'Natural disasters', value: '4' },
+    { label: 'Health emergencies', value: '5' },
+    { label: 'Accommodation issues', value: '6' },
+    { label: 'Protests', value: '7' },
+    { label: 'Strikes', value: '8' },
+    { label: 'Security threats', value: '9' },
+    { label: 'Animal-related incidents', value: '10' },
+    { label: 'Financial issues', value: '11' }
+  ]
+
+  const countryObj = {
+    country_name: country,
+  };
+
+  const id = traveler.traveler_id;
+  const [details, setDetails] = useState('');
+  const [eventStatus, setEventStatus] = useState('true');
+  const [picture, setPicture] = useState('#');
+  const [stackholderId, setStackholderId] = useState('null');
+  const [serialTypeNumber, setSerialTypeNumber] = useState('');
+  const [countryNumber, setCountryNumber] = useState('');
+  const [areaNumber, setAreaNumber] = useState('');
+  const [selectedSerialType, setSelectedSerialType] = useState(null);
+
+
+  useEffect(() => {
+    //insert the API Key
+    Geocoder.init('AIzaSyDN2je5f_VeKV-DCzkaYBg1nRs_N6zn5so');
+    Geocoder.from(userLocation.coords.latitude, userLocation.coords.longitude)
+      .then(json => {
+        const addressComponents = json.results[0].address_components;
+        const countryComponent = addressComponents.find(component => component.types.includes('country'));
+        const regionComponent = addressComponents.find(component => component.types.includes('administrative_area_level_1'));
+
+        // const continentComponent = addressComponents.find(component => component.types.includes('continent'));
+        setCountry(countryComponent.long_name);
+        setRegion(regionComponent.long_name);
+        addContry();
+        
+      })
+      .catch(error => console.warn(error))
+  }, []);
+
+  console.log('1 contry:', { country })
+  console.log('2 region:', {region })
+
+  const newEvent = {
+    Details: details,
+    event_date: new Date().toISOString().slice(0, 10),
+    event_time: `${new Date().getHours()}:${new Date().getMinutes()}`,
+    Latitude: userLocation.coords.latitude,
+    Longitude: userLocation.coords.longitude,
+    event_status: eventStatus,
+    Picture: picture,
+    TravelerId: id,
+    StackholderId: stackholderId,
+    serialTypeNumber: serialTypeNumber,
+    country_number: countryNumber,
+    area_number: areaNumber,
+  };
+
+  console.log('3  new', newEvent);
+
+   addContry = () => {
+    console.log('4  *****',{country})
+    fetch('http://cgroup90@194.90.158.74/cgroup90/prod/api/post/country', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(countryObj),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('5  ********', { data })
+        setCountryNumber(data)
+        addRegion();
+      }
+      )
+      .catch(error => {
+        console.error(error);
+        console.log('6 Error');
+      });
+  }
+
+   addRegion = () => {
+    const areaObj = {
+      country_number: countryNumber,
+      area_name: region
+    }
+    console.log('7  *****',{areaObj})
+    fetch('http://cgroup90@194.90.158.74/cgroup90/prod/api/post/area', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(areaObj),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('8  ********', { data })
+        setAreaNumber(data)
+      }
+      )
+      .catch(error => {
+        console.error(error);
+        console.log('Error');
+      });
+  }
+  const createEvent = async () => {
+
+    if (newEvent.Details === '' || newEvent.serialTypeNumber === '') {
+      alert('Please enter details and type');
+    }
+    else {
+      // Send a POST request to your backend API with the event data
+      fetch('http://cgroup90@194.90.158.74/cgroup90/prod/api/post/newevent', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newEvent),
+      })
+        .then(response => response.json())
+        .then(data => {
+          // Handle the response data as needed
+          console.log("9", data);
+           alert('Publish')
+          navigation.goBack(); // Navigate back to the "Around You" screen
+          //console.log({ newEvent })
+         
+
+        })
+        .catch(error => {
+          console.error(error);
+          alert('Error', error);
+        });
+    }
+  }
+
+
+  return (
+    < GradientBackground>
+
+      <ScrollView>
+        <View style={styles.container}>
+          <Image source={RoadRanger} style={styles.RoadRanger} />
+          <Text style={styles.text}>What Happend:</Text>
+          <TextInput style={styles.input}
+            value={details}
+            onChangeText={(text) => setDetails(text)}
+            placeholder="Write here..."
+            multiline
+            spellCheck="true"
+            onSubmitEditing={() => {
+              //close the keyboard
+              TextInput.State.blur(TextInput.State.currentlyFocusedInput())
+            }}>
+          </TextInput>
+          <Text style={styles.text}>Type:</Text>
+
+          <Dropdown
+            style={styles.dropdown}
+            placeholderStyle={styles.placeholderStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            data={serialType}
+            maxHeight={300}
+            labelField="label"
+            valueField="value"
+            placeholder={"Select type of event"}
+            value={selectedSerialType}
+            onChange={item => {
+              setSerialTypeNumber(item.value)
+              setSelectedSerialType(item) // Update the selected item state variable
+
+            }} />
+
+          <TouchableOpacity style={styles.photo} >
+            <Icon name="camera-outline" style={styles.icon} size={30} color={'white'} />
+            <Text style={styles.btnText}>
+              Add Photo
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.btnSave} onPress={createEvent}>
+            <Text style={styles.btnText}>
+              Publish
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+      </ScrollView >
+    </GradientBackground>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    marginVertical: 10,
+    marginHorizontal: 10,
+    padding: 20,
+    width: "100%",
+
+  },
+
+  RoadRanger: {
+    alignSelf: 'center',
+    resizeMode: 'contain',
+    height: 100,
+    marginBottom: 20
+
+  },
+  text: {
+    color: '#144800',
+    fontSize: 20,
+
+  },
+
+
+  btnText: {
+    color: '#F8F8FF',
+    alignSelf: 'center',
+    fontSize: 20,
+
+  },
+
+  dropdown: {
+    height: 40,
+    borderColor: '#8FBC8F',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    borderColor: '#144800',
+    borderWidth: 1,
+    borderRadius: 25,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginBottom: 10,
+    marginTop: 10,
+    width: "90%",
+
+  },
+  text1: {
+    fontSize: 18,
+    alignSelf: 'center',
+    color: "#A9A9A9"
+  },
+
+  input: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    width: "90%",
+    fontSize: 20,
+    paddingVertical: 70,
+    paddingHorizontal: 15,
+    borderColor: '#144800',
+    borderWidth: 1,
+    borderRadius: 25,
+
+
+  },
+  photo: {
+    marginVertical: 20,
+    width: "80%",
+    alignSelf: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderColor: '#144800',
+    borderWidth: 2,
+    borderRadius: 25,
+    backgroundColor: '#144800',
+    marginBottom: 50,
+    flexDirection: 'row',
+
+  },
+  icon: {
+    left: 30,
+    size: 30,
+    marginRight: 50
+
+  },
+  // label: {
+  //   position: 'absolute',
+  //   backgroundColor: 'white',
+  //   left: 22,
+  //   top: 8,
+  //   zIndex: 999,
+  //   paddingHorizontal: 8,
+  //   fontSize: 14,
+
+
+  // },
+  placeholderStyle: {
+    fontSize: 18,
+    color: "#A9A9A9"
+  },
+  selectedTextStyle: {
+    fontSize: 18,
+  },
+  btnSave: {
+    marginVertical: 20,
+    width: "50%",
+    alignSelf: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderColor: '#144800',
+    borderWidth: 2,
+    borderRadius: 25,
+    backgroundColor: '#144800'},
+});
+
+
+
