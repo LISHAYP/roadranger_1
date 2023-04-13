dc9743190f215d4034a41db217dec9bae3903887
lielduenias <117348454+lielduenias@users.noreply.github.com>
2023-04-13 22:00:45 +0300

    13/04/2023 22:00

--- server/WebApplication1/Controllers/SearchEventController.cs
+++ server/WebApplication1/Controllers/SearchEventController.cs
@@ -190,6 +190,76 @@ namespace WebApplication1.Controllers
 
             return Ok(events);
         }
+        [HttpPost]
+        [Route("api/post/searchByParameters")]
+        public IHttpActionResult SearchByParameters([FromBody] List<EventParam> searchParams)
+        {
+            if (searchParams == null)
+            {
+                // Return a BadRequest response to indicate that the caller did not provide valid search parameters
+                return BadRequest("Invalid search parameters");
+            }
+
+            var events = db.tblEvents.AsQueryable();
+
+            foreach (var param in searchParams)
+            {
+                switch (param.Name.ToLower())
+                {
+                    case "countrynumber":
+                        if (int.TryParse(param.Value, out var countryNumber))
+                        {
+                            events = events.Where(x => x.country_number == countryNumber);
+                        }
+                        break;
+                    case "areanumber":
+                        if (int.TryParse(param.Value, out var areaNumber))
+                        {
+                            events = events.Where(x => x.area_number == areaNumber);
+                        }
+                        break;
+                    case "serialtypenumber":
+                        if (int.TryParse(param.Value, out var serialTypeNumber))
+                        {
+                            events = events.Where(x => x.serialTypeNumber == serialTypeNumber);
+                        }
+                        break;
+                    case "eventdate":
+                        if (DateTime.TryParse(param.Value, out var eventDate))
+                        {
+                            events = events.Where(x => x.event_date == eventDate);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            var result = events.Select(x => new EventDto
+            {
+                eventNumber = x.eventNumber,
+                Details = x.details,
+                EventDate = x.event_date,
+                EventTime = x.event_time,
+                Latitude = x.latitude,
+                Longitude = x.longitude,
+                EventStatus = x.event_status,
+                Picture = x.picture,
+                TravelerId = x.travelerId,
+                StackholderId = x.stackholderId,
+                SerialTypeNumber = x.serialTypeNumber,
+                CountryNumber = x.country_number,
+                AreaNumber = x.area_number
+            })
+                                .ToList();
+
+            if (result.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
 
 
         // PUT: api/SearchEvent/5
