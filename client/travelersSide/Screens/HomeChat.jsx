182a9258d93a55737ae56a32ff9e81f2dac78d72
lishayp <LISHAY.PELED@GMAIL.COM>
2023-04-15 23:37:39 +0300

    Update HomeChat.jsx

--- client/travelersSide/Screens/HomeChat.jsx
+++ client/travelersSide/Screens/HomeChat.jsx
@@ -33,3 +33,199 @@ const styles = StyleSheet.create({
         marginTop:50
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { StyleSheet, Text, View, TouchableOpacity, useState } from 'react-native'
+// import React from 'react'
+// import GradientBackground from '../Components/GradientBackground';
+// import { useFocusEffect, useNavigation } from "@react-navigation/native";
+// import {
+//   getDatabase,
+//   get,
+//   ref,
+//   set,
+//   onValue,
+//   push,
+//   update,
+// } from 'firebase/database';
+
+// const HomeChat = (props) => {
+//   const navigation = useNavigation();
+//   const traveler = props.route.params;
+//   const [currentPage, setCurrentPage] = useState('login');
+//   const [username, setUsername] = useState(`${traveler.first_name}`);
+//   const [users, setUsers] = useState([]);
+//   const [userToAdd, setUserToAdd] = useState(null);
+//   const [selectedUser, setSelectedUser] = useState(null);
+//   const [myData, setMyData] = useState(null);
+
+//   const onLogin = async () => {
+//     try {
+//       const database = getDatabase();
+//       //first check if the user registered before
+
+//       const user = await findUser(username);
+//       const findUser = async name => {
+//         const database = getDatabase();
+//         const mySnapshot = await get(ref(database, `users/${name}`));
+//         return mySnapshot.val();
+//       };
+
+//       //create a new user if not registered
+//       if (user) {
+//         setMyData(user);
+//       } else {
+//         const newUserObj = {
+//           username: username,
+//           avatar: 'https://i.pravatar.cc/150?u=' + Date.now(),
+//         };
+
+//         set(ref(database, `users/${username}`), newUserObj);
+//         setMyData(newUserObj);
+//       }
+
+//       // set friends list change listener
+//       const myUserRef = ref(database, `users/${username}`);
+//       onValue(myUserRef, snapshot => {
+//         const data = snapshot.val();
+//         setUsers(data.friends);
+//         setMyData(prevData => ({
+//           ...prevData,
+//           friends: data.friends,
+//         }));
+//       });
+//       setCurrentPage('users');
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+
+//   const onClickUser = user => {
+//     setCurrentPage('chat');
+//     setSelectedUser(user);
+//   };
+
+//   const onAddFriend = async name => {
+//     try {
+//       //find user and add it to my friends and also add me to his friends
+//       const database = getDatabase();
+
+//       const user = await findUser(name);
+
+//       if (user) {
+//         if (user.username === myData.username) {
+//           // don't let user add himself
+//           return;
+//         }
+
+//         if (
+//           myData.friends &&
+//           myData.friends.findIndex(f => f.username === user.username) > 0
+//         ) {
+//           // don't let user add a user twice
+//           return;
+//         }
+
+//         // create a chatroom and store the chatroom id
+
+//         const newChatroomRef = push(ref(database, 'chatrooms'), {
+//           firstUser: myData.username,
+//           secondUser: user.username,
+//           messages: [],
+//         });
+
+//         const newChatroomId = newChatroomRef.key;
+
+//         const userFriends = user.friends || [];
+//         //join myself to this user friend list
+//         update(ref(database, `users/${user.username}`), {
+//           friends: [
+//             ...userFriends,
+//             {
+//               username: myData.username,
+//               avatar: myData.avatar,
+//               chatroomId: newChatroomId,
+//             },
+//           ],
+//         });
+
+//         const myFriends = myData.friends || [];
+//         //add this user to my friend list
+//         update(ref(database, `users/${myData.username}`), {
+//           friends: [
+//             ...myFriends,
+//             {
+//               username: user.username,
+//               avatar: user.avatar,
+//               chatroomId: newChatroomId,
+//             },
+//           ],
+//         });
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   const onBack = () => {
+//     setCurrentPage('users');
+//   };
+
+//   switch (currentPage) {
+//     case 'login':
+//       return (
+//         <View style={styles.container}>
+//           < GradientBackground>
+//             <Text>HomeChat</Text>
+//             <TouchableOpacity style={styles.btnSave} onPress={() => { navigation.navigate("Chat", traveler) }}>
+//               <Text style={styles.btnText}>
+//                 Save Changes
+//               </Text>
+//             </TouchableOpacity>
+//           </GradientBackground>
+//         </View>
+
+//       );
+//     case 'users':
+//       return (
+//         <Users
+//           users={users}
+//           onClickUser={onClickUser}
+//           userToAdd={userToAdd}
+//           setUserToAdd={setUserToAdd}
+//           onAddFriend={onAddFriend}
+//         />
+//       );
+//     case 'chat':
+//       return (
+//         <Chat myData={myData} selectedUser={selectedUser} onBack={onBack} />
+//       );
+//     default:
+//       return null;
+//   }
+// }
+
+// export default HomeChat
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     marginTop: 50
+//   }
+// })
+
