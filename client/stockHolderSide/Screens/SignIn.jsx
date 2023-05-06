420aa1d863fea9b27e4d5872c740fe77cb608553
lishayp <LISHAY.PELED@GMAIL.COM>
2023-05-06 17:46:16 +0300

    push notifications

--- client/stockHolderSide/Screens/SignIn.jsx
+++ client/stockHolderSide/Screens/SignIn.jsx
@@ -1,15 +1,19 @@
 import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native';
-import React from 'react'
 import RoadRanger from '../assets/RoadRanger.png';
 import Icon from "react-native-vector-icons/Ionicons";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
-import { useState } from 'react';
 import GradientBackground from '../Components/GradientBackground';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { useRef } from 'react';
+import { Button, Platform } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 export default function SignIn() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loginFailed, setLoginFailed] = useState(false);
+    const [devaiceToken, setDevaiceToken] = useState('');
 
 
     const handleLogin = () => {
@@ -45,6 +49,68 @@ console.log("********",stakeholder);
     state = {
         showPassword: false
     };
+
+    async function registerForPushNotificationsAsync() {
+        let token;
+        if (Device.isDevice) {
+            const { status: existingStatus } = await Notifications.getPermissionsAsync();
+            let finalStatus = existingStatus;
+            if (existingStatus !== 'granted') {
+                const { status } = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+            if (finalStatus !== 'granted') {
+                alert('Failed to get push token for push notification!');
+                return;
+            }
+            token = (await Notifications.getExpoPushTokenAsync()).data;
+            setDevaiceToken(token);
+            console.log(token);
+        } else {
+            alert('Must use physical device for Push Notifications');
+        }
+
+        if (Platform.OS === 'android') {
+            Notifications.setNotificationChannelAsync('default', {
+                name: 'default',
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: '#FF231F7C',
+            });
+        }
+
+        return token;
+    }
+    Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+            shouldShowAlert: true,
+            shouldPlaySound: false,
+            shouldSetBadge: false,
+        }),
+    });
+
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
+    const notificationListener = useRef();
+    const responseListener = useRef();
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification);
+        });
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response);
+        });
+
+        return () => {
+            Notifications.removeNotificationSubscription(notificationListener.current);
+            Notifications.removeNotificationSubscription(responseListener.current);
+        };
+    }, []);
+
     return (
         < GradientBackground>
 
