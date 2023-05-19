4ec9ebd1962d8f7f3a9611fcd0030c09572d91f2
Orian Nahari <orian.nahari1@gmail.com>
2023-05-19 18:14:20 +0300

    kkkk

--- client/travelersSide/Screens/AroundYou.jsx
+++ client/travelersSide/Screens/AroundYou.jsx
@@ -191,7 +191,7 @@ export default function AroundYou(props) {
                                 title="My Location"
                                 description="This is my current location"
                             />
-                            {Events.filter(event => event.event_status !== false).map(event => (
+                            {Events.filter(event => event.event_status !== false && event.is_related==null  ).map(event => (
                                 <Marker
                                     key={event.EventNumber}
                                     coordinate={{
