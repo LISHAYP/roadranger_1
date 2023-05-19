f94f4c04d956c4e262ee68aa1dabfce951650c85
lishayp <LISHAY.PELED@GMAIL.COM>
2023-05-19 18:03:56 +0300

    Update AroundYou.jsx

--- client/travelersSide/Screens/AroundYou.jsx
+++ client/travelersSide/Screens/AroundYou.jsx
@@ -41,8 +41,8 @@ export default function AroundYou(props) {
             })
                 .then(response => response.json())
                 .then(data => {
-                    setLasteventOfTraveler(data)
-                    console.log(data);
+                    setLasteventOfTraveler(data.lastEventId);
+                    console.log("++++",data.lastEventId);
                 }
                 )
                 .catch(error => {
@@ -129,6 +129,7 @@ export default function AroundYou(props) {
             is_related: eventNumber
         }
         console.log("**************", updateEventObj);
+        console.log("*****-*********", lasteventOfTraveler);
 
         fetch(`http://cgroup90@194.90.158.74/cgroup90/prod/api/put/updateevent/${lasteventOfTraveler}`, {
             method: 'PUT',
@@ -141,7 +142,8 @@ export default function AroundYou(props) {
             .then((response) => response.json())
             .then((data) => {
                 console.log(data); // Traveler updated successfully.
-                // Alert.alert('Event updated successfully')
+                setModalVisible(false);
+                setLasteventOfTraveler('');
             })
             .catch((error) => {
                 console.error(error);
@@ -305,17 +307,17 @@ export default function AroundYou(props) {
                                    
                                         {/* Modal content */}
                                         <Text>Did u mean this event?</Text>
-                                         <TouchableOpacity style={styles.btnLogIn} onPress={() => relatedEvent(matchedEvent.eventNumber)}>
+                                      
+                                        <Text>{matchedEvent.Details}</Text>
+                                        <Image style={styles.user} source={{ uri: matchedEvent.Picture }} />
+
+                                        <Text>Event Number: {matchedEvent.eventNumber}</Text>
+                                        <TouchableOpacity style={styles.btnLogIn} onPress={() => relatedEvent(matchedEvent.eventNumber)}>
                                             <Text>Yes</Text>
                                         </TouchableOpacity>
                                         <TouchableOpacity style={styles.btnLogIn} onPress={() => setModalVisible(false)} >
                                             <Text>No</Text>
                                         </TouchableOpacity> 
-                                        <Text>{matchedEvent.Details}</Text>
-                                        <Image style={styles.user} source={{ uri: matchedEvent.Picture }} />
-
-                                        <Text>Event Number: {matchedEvent.eventNumber}</Text>
-                                     
                                         {/* Add more Text components for other parameters */}
                                     
                                 </View>
