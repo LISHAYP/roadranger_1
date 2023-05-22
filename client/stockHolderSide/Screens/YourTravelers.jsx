4d7ecb58f8bdccbcb60fcdbfb6cb758902e0a23d
Orian Nahari <orian.nahari1@gmail.com>
2023-05-22 15:14:01 +0300

    location

--- client/stockHolderSide/Screens/YourTravelers.jsx
+++ client/stockHolderSide/Screens/YourTravelers.jsx
@@ -19,84 +19,92 @@ export default function YourTravelers(props) {
   const [myTravelers, setMyTravelers] = useState([])
   console.log(stakeholder)
   useEffect(() => {
-    stackholderType()
+    console.log("----------",stakeholder.StakeholderType)
+    if (stakeholder.StakeholderType == 'Insurance Company') {
+      console.log("----------","Insurance Company" )
+
+      stackholderTypeInsurence()
+    }
+    else {
+      stackholderType()
+    }
   }, []);
 
   const stackholderType = () => {
-    if (stakeholder.StakeholderType == 'Insurance Company') {
-      const objInsuranceCompany = {
-        insurence_company: stakeholder.StakeholderName
-      }
-     
-      fetch('http://cgroup90@194.90.158.74/cgroup90/prod/api/post/GetTravelersByInsuranceCompany', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(objInsuranceCompany),
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log("^^^^^", data)
-          // Map over the data and get the address for each traveler
-          Promise.all(data.map(traveler => {
-            if (!traveler.last_location.Latitude) {
-              return Promise.reject(new Error('Traveler does not have a location'));
-            }
-            const lat = traveler.last_location.Latitude;       
-            const lng = traveler.last_location.Longitude;
-            return Geocoder.from(lat, lng).then(json => {
-              const location = json.results[0].address_components;
-              const number = location[0].long_name;
-              const street = location[1].long_name;
-              const city = location[2].long_name;
-              const address = `${street} ${number}, ${city}`;
-              return { ...traveler, address };
-            }).catch(error => {
-              console.error(error);
-              // handle the error appropriately
-            });
-          })).then(travelersWithAddress => {
-            setMyTravelers(travelersWithAddress);
+    fetch('http://cgroup90@194.90.158.74/cgroup90/prod/api/lastlocation', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
 
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("%%%%%%%%%%%%%%", data)
+        // Map over the data and get the address for each traveler
+        Promise.all(data.map(traveler => {
+          const lat = traveler.last_location.Latitude;
+          const lng = traveler.last_location.Longitude;
+          return Geocoder.from(lat, lng).then(json => {
+            const location = json.results[0].address_components;
+            const number = location[0].long_name;
+            const street = location[1].long_name;
+            const city = location[2].long_name;
+            const address = `${street} ${number}, ${city}`;
+            return { ...traveler, address };
+          });
+        })).then(travelersWithAddress => {
+          setMyTravelers(travelersWithAddress);
+
+        });
+      });
+
+  }
+
+
+  const stackholderTypeInsurence = () => {
+    const objInsuranceCompany = {
+      insurence_company: stakeholder.StakeholderName
+    }
+    console.log("============",objInsuranceCompany)
+    fetch('http://cgroup90@194.90.158.74/cgroup90/prod/api/post/GetTravelersByInsuranceCompany', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(objInsuranceCompany),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("^^^^^", data)
+        // Map over the data and get the address for each traveler
+        Promise.all(data.map(traveler => {
+          const lat = traveler.last_location.Latitude;
+          const lng = traveler.last_location.Longitude;
+          return Geocoder.from(lat, lng).then(json => {
+            const location = json.results[0].address_components;
+            const number = location[0].long_name;
+            const street = location[1].long_name;
+            const city = location[2].long_name;
+            const address = `${street} ${number}, ${city}`;
+            return { ...traveler, address };
           }).catch(error => {
             console.error(error);
             // handle the error appropriately
           });
-        });
-    }
-    if (stakeholder.StakeholderType != 'Insurance Company') {
-      fetch('http://cgroup90@194.90.158.74/cgroup90/prod/api/lastlocation', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
+        })).then(travelersWithAddress => {
+          setMyTravelers(travelersWithAddress);
 
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log("%%%%%%%%%%%%%%", data)
-          // Map over the data and get the address for each traveler
-          Promise.all(data.map(traveler => {
-            const lat = traveler.last_location.Latitude;       
-            const lng = traveler.last_location.Longitude;
-            return Geocoder.from(lat, lng).then(json => {
-              const location = json.results[0].address_components;
-              const number = location[0].long_name;
-              const street = location[1].long_name;
-              const city = location[2].long_name;
-              const address = `${street} ${number}, ${city}`;
-              return { ...traveler, address };
-            });
-          })).then(travelersWithAddress => {
-            setMyTravelers(travelersWithAddress);
-
-          });
+        }).catch(error => {
+          console.error(error);
+          // handle the error appropriately
         });
-    }
+      });
   }
+
+
 
   function formatDateTime(isoDateTime) {
     const date = new Date(isoDateTime);
@@ -154,10 +162,11 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2},
-      shadowOpacity: 0.23,
-      shadowRadius: 2.62,
-      elevation: 4
+      height: 2
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4
   },
   event: {
     flexDirection: 'row',
