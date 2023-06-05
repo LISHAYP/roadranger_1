bdc7aec9962901728796d62fff84b025630759ba
lishayp <LISHAY.PELED@GMAIL.COM>
2023-06-05 11:44:35 +0300

    .

--- client/travelersSide/Screens/NewEvent.jsx
+++ client/travelersSide/Screens/NewEvent.jsx
@@ -8,9 +8,8 @@ import GradientBackground from '../Components/GradientBackground';
 import Geocoder from 'react-native-geocoding';
 import { useEffect } from 'react';
 import BackButton from '../Components/BackButton';
-import * as Notifications from 'expo-notifications';
-import { auth } from '../firebase';
-import Navbar from '../Components/Navbar';
+import stringSimilarity from 'string-similarity';
+
 
 export default function NewEvent(props) {
   const traveler = props.route.params.traveler;
@@ -37,14 +36,14 @@ export default function NewEvent(props) {
   const id = traveler.traveler_id;
   const [details, setDetails] = useState('');
   const [eventStatus, setEventStatus] = useState('true');
-  const [picture, setPicture] = useState('#');
+  const [picture, setPicture] = useState('http://cgroup90@194.90.158.74/cgroup90/prod/profilePictures/no-image.png');
   const [stackholderId, setStackholderId] = useState('null');
   const [serialTypeNumber, setSerialTypeNumber] = useState('');
   const [countryNumber, setCountryNumber] = useState('');
   const [areaNumber, setAreaNumber] = useState('');
   const [selectedSerialType, setSelectedSerialType] = useState(null);
   const [relatedEvents, setRelatedEvents] = useState('');
-
+  const [setEntitiy, setSetEntitiy] = useState(false);
   useEffect(() => {
     //insert the API Key
     Geocoder.init('AIzaSyDN2je5f_VeKV-DCzkaYBg1nRs_N6zn5so');
@@ -129,7 +128,12 @@ export default function NewEvent(props) {
         console.log('Error');
       });
   }
+  const translations = []; // Array to store translations
+  const entities = [];
+
+
   const createEvent = async () => {
+
     if (newEvent.Details === '' || newEvent.serialTypeNumber === '') {
       Alert.alert('Please enter details and type');
     } else {
@@ -163,7 +167,7 @@ export default function NewEvent(props) {
             .then(data1 => {
               const relatedEventsData = data1; // Assign the data to a constant variable
               const matchedEvents = []; // Array to store matched events
-  
+
               for (let i = 0; i < relatedEventsData.length; i++) {
                 const event = relatedEventsData[i];
                 if (compareLabels(event, newEvent)) {
@@ -249,8 +253,9 @@ export default function NewEvent(props) {
                               throw new Error('Text comparison request failed');
                             }
                             const data = await response.json();
+                            for (const entit of data.entities) {
                               entityname = data.entities[0].name;
-                            
+                            }
                             //console.log('entityname', entityname);
                             translations.forEach(async translationData => {
                               const requestBody = {
@@ -280,9 +285,9 @@ export default function NewEvent(props) {
                                 }
                                 entities.push(entObj);
                               }
-                              if(entityname) {
-                                 console.log("here in entityname", entityname)
-                                console.log('entities', entities);
+                              if (entityname) {
+                                // console.log("here in entityname", entityname)
+                                // console.log('entities', entities);
                                 for (let i = 0; i < entities.length; i++) {
                                   const similarity = stringSimilarity.compareTwoStrings(entities[i].name, entityname);
 
@@ -306,7 +311,7 @@ export default function NewEvent(props) {
                                         (error) => {
                                           console.log("err post=", error);
                                         }, []);
-                                    break; // Exit the loop after finding a match
+                                        break; // Exit the loop after finding a match
                                   }
                                 }
                               }
@@ -335,7 +340,7 @@ export default function NewEvent(props) {
               }
               Alert.alert('Publish');
               const data = traveler;
-              navigation.navigate("Around You", { data, matchedEvents});
+              navigation.navigate("Around You", { data, matchedEvents });
             })
             .catch(error => {
               console.error(error);
@@ -348,21 +353,24 @@ export default function NewEvent(props) {
         });
     }
   };
-  
+
   const compareLabels = (event1, event2) => {
     if (!event1.labels || !event2.labels) {
       // If either event is missing the labels property, return false
+
       return false;
     }
-  
-    if ( event1.Details === event2.Details) {
+
+    if (event1.Details === event2.Details) {
       // If Details are defined and identical, return false
       return false;
     }
-  
-    const labels1 = JSON.parse(event1.labels).map(label => label.description);
-    const labels2 = JSON.parse(event2.labels).map(label => label.description);
-  
+
+
+    const labels1 = JSON.parse(event1.labels).filter(label => label.score > 0.5).map(label => label.description);
+    const labels2 = JSON.parse(event2.labels).filter(label => label.score > 0.5).map(label => label.description);
+
+
     for (const label1 of labels1) {
       for (const label2 of labels2) {
         if (label1 === label2) {
@@ -370,13 +378,15 @@ export default function NewEvent(props) {
         }
       }
     }
-  
+
     return false;
   };
-  
-  
-  
-  
+
+
+
+
+
+
 
   const OpenCameraE = () => {
     navigation.navigate('CameraE', { idE: `${new Date().getHours()}:${new Date().getMinutes()}_${new Date().toISOString().slice(0, 10)}`, userLocation, traveler });
@@ -386,12 +396,9 @@ export default function NewEvent(props) {
 
   return (
     < GradientBackground>
-      <BackButton text="New Post" />
-
-      <Navbar traveler={traveler} userLocation={userLocation} />
       <ScrollView>
         <View style={styles.container}>
-
+          <BackButton />
           <Image source={RoadRanger} style={styles.RoadRanger} />
           <Text style={styles.text}>What Happend:</Text>
           <TextInput style={styles.input}
@@ -443,11 +450,12 @@ export default function NewEvent(props) {
 }
 const styles = StyleSheet.create({
   container: {
-    marginTop: 120,
+    marginTop: 20,
     marginVertical: 10,
     marginHorizontal: 10,
     padding: 20,
     width: "100%",
+    alignSelf: 'center'
 
   },
 
@@ -455,13 +463,21 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     resizeMode: 'contain',
     height: 100,
-    marginBottom: 20
+    marginBottom: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9
 
   },
   text: {
     color: '#144800',
     fontSize: 20,
-
+    left: 15,
   },
   btnText: {
     color: '#F8F8FF',
@@ -471,52 +487,54 @@ const styles = StyleSheet.create({
   },
 
   dropdown: {
+    alignSelf: 'center',
     height: 40,
     borderColor: '#8FBC8F',
     borderWidth: 0.5,
-    borderRadius: 8,
     paddingHorizontal: 8,
     borderColor: '#144800',
     borderWidth: 1,
-    borderRadius: 25,
+    borderRadius: 15,
     paddingVertical: 10,
     paddingHorizontal: 15,
     marginBottom: 10,
     marginTop: 10,
-    width: "90%",
+    width: "95%",
 
   },
-  text1: {
-    fontSize: 18,
-    alignSelf: 'center',
-    color: "#A9A9A9"
-  },
-
   input: {
+    alignSelf: 'center',
     flexDirection: 'row',
     marginVertical: 10,
-    width: "90%",
+    width: "100%",
     fontSize: 20,
     paddingVertical: 70,
     paddingHorizontal: 15,
     borderColor: '#144800',
     borderWidth: 1,
-    borderRadius: 25,
-
+    borderRadius: 15,
 
   },
   photo: {
     marginVertical: 20,
-    width: "80%",
+    width: "70%",
     alignSelf: 'center',
     paddingVertical: 10,
     paddingHorizontal: 15,
     borderColor: '#144800',
     borderWidth: 2,
-    borderRadius: 25,
+    borderRadius: 20,
     backgroundColor: '#144800',
-    marginBottom: 50,
+    marginBottom: 40,
     flexDirection: 'row',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9
 
   },
   icon: {
@@ -544,15 +562,24 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   btnSave: {
+    height: 55,
     marginVertical: 20,
-    width: "50%",
+    width: "55%",
     alignSelf: 'center',
     paddingVertical: 10,
     paddingHorizontal: 15,
     borderColor: '#144800',
     borderWidth: 2,
-    borderRadius: 25,
-    backgroundColor: '#144800'
+    borderRadius: 20,
+    backgroundColor: '#144800',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9
   },
 });
 
